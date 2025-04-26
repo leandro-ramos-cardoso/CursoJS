9e9d6c87ca1a94a6e5e157f5ec61357b863ffc58
Leandro Ramos Cardoso <lrcardoso.jp@gmail.com>
2025-04-26 19:33:47 -0300

    feature condicional

---
+++ Módulo D - Condicionais/ex008.js
@@ -0,0 +1,9 @@
+// CONDICIONAL SIMPLES (NÃO TEM O ELSE) COM IF
+var velocidade = 78.5;
+
+console.log(`A velocidade do seu carro é ${velocidade} Km/h`);
+if(velocidade > 60){
+    console.log(`Sua velocidade foi acima da permitida. MULTADO!`);
+}
+
+console.log('Digija sempre usando cinto de segurança!');
